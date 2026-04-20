e322a249e57b7ef724f92df0dd0cfe1020e6d195
Jean <jean.faure2005@gmail.com>
2026-04-20 16:22:16 +0200

    ajout d'une figure de projection 2d

--- 3D plot test.py
+++ 3D plot test.py
@@ -9,17 +9,31 @@ y = np.random.rand(50)
 z = np.random.rand(50)
 
 # Tailles n aléatoire
-n = np.random.rand(50)*50
+n = 50 # np.random.rand(50)*50
 
-# Creating a 3D figure
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(1, 2, 1, projection='3d')
+fig = plt.figure(figsize=(12, 6))
 
+# Left: Creating a 3D figure
 
+ax1 = fig.add_subplot(1, 2, 1, projection='3d')
 # Creating a 3D scatter plot
-scatter = ax.scatter(x, y, z, c=z, cmap='viridis', s=n, edgecolor='k')
+ax1.scatter(x, y, z, c=z, cmap='viridis', s=n, edgecolor='k')
+ax1.set_title("3D points")
 
-plt.plot(x,y, 0,'o')
 
-# Display the plot (rotate manually with mouse)
-plt.show()
+
+# Rigth: projection 2D
+
+# plt.plot(x,y, 0,'o')
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.scatter(x, y, c=z, cmap='viridis', s=n, edgecolor='k')
+ax2.set_title("Projection XY")
+ax2.set_xlabel("x")
+ax2.set_ylabel("y")
+ax2.axis("equal")
+
+if __name__ == "__main__":
+    # Display the plot (rotate manually with mouse)
+    plt.tight_layout()
+    plt.show()
