26fd0de8cd6b7a084e5801f6e13f8f5fa5aa340b
ClemBelaid <clementine.belaid@etu.univ-grenoble-alpes.fr>
2026-04-21 17:26:07 +0200

    Ajout de la projection de points 3D sur un plan 2D (en rouge). 21/04

--- 3D plot test.py
+++ 3D plot test.py
@@ -1,25 +1,50 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import math
 
-# Genère 50 points (x,y,z) aléatoires
+pi = math.pi
+
+# n le nombre de billes à générer
+n = 15
+
+# Genère n points (x,y,z) aléatoires
 np.random.seed(42)
-x = np.random.rand(50)
-y = np.random.rand(50)
-z = np.random.rand(50)
+x = np.random.rand(n)
+y = np.random.rand(n)
+z = np.random.rand(n)
 
-# Tailles n aléatoire
-n = np.random.rand(50)*50
+# Tailles s aléatoires
+s = np.random.rand(n)*60 + 30
 
-# Creating a 3D figure
-fig = plt.figure(figsize=(8, 6))
+# Projection d'un vecteur u sur un plan P de vecteur normal n
+def proj(v, n):
+    n_norm = np.sqrt(sum(n**2))
+    v_prime = np.dot(v,n)/np.dot(n,n)*n
+    return v - v_prime
+
+# Création de la figure 3D
+fig = plt.figure(figsize=(16, 10))
 ax = fig.add_subplot(1, 2, 1, projection='3d')
+plt.xlabel("X")
+plt.ylabel("Y")
 
+# Création d'un scatter plot
+ax.scatter(x, y, z, c=z, cmap='viridis', s = s, edgecolor='k')
 
-# Creating a 3D scatter plot
-scatter = ax.scatter(x, y, z, c=z, cmap='viridis', s=n, edgecolor='k')
+# Vecteur normal à un plan P quelconque (pour tester)
+normal = np.array([1, 1, 1])
 
-plt.plot(x,y, 0,'o')
+# Calcul des coordonnées du plan
+d = 0 # Ou bien d = 0
 
-# Display the plot (rotate manually with mouse)
+xx, yy = np.meshgrid(np.linspace(0,1,20), np.linspace(0,1,20))
+zz = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2] # z = (-ax -by)/c
+ax.plot_surface(xx, yy, zz, alpha=0.2)
+
+for i in range(n):
+    v = (x[i], y[i], z[i])
+    v_proj = proj(v,normal)
+    ax.scatter(v_proj[0], v_proj[1], v_proj[2], c ="red", s = s[i], edgecolor='k')
+
 plt.show()
