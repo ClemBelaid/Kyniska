ea8cb0bdc663e87d6a879cf6af56ec9e70651195
Haha892 <haiban-aladel.alshouni@etu.univ-grenoble-alpes.fr>
2026-04-22 09:55:38 +0200

    Update mire2.py
    Added Mire class with JSON save/load functionality.

--- mire2.py
+++ mire2.py
@@ -1,3 +1,53 @@
+import json
+import numpy as np
+
+class Mire:
+    def __init__(self, points, ids=None):
+        self.points = np.array(points, dtype=float)
+
+        if ids is None:
+            self.ids = np.arange(len(points))
+        else:
+            self.ids = np.array(ids)
+
+    def __len__(self):
+        return len(self.points)
+
+    def copy(self):
+        return Mire(self.points.copy(), self.ids.copy())
+
+    def save_json(self, filename):
+        data = {
+            "name": filename,
+            "points": []
+        }
+
+        for pid, p in zip(self.ids, self.points):
+            data["points"].append({
+                "id": int(pid),
+                "x": float(p[0]),
+                "y": float(p[1]),
+                "z": float(p[2])
+            })
+
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+
+    @classmethod
+    def load_json(cls, filename):
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        points = []
+        ids = []
+
+        for pt in data["points"]:
+            ids.append(pt["id"])
+            points.append([pt["x"], pt["y"], pt["z"]])
+
+        return cls(points, ids, data.get("name", "mire"))
+
+
 def generer_cone_tronque(cls, nb_billes, rayon_base=100.0, rayon_sommet=50.0, hauteur=30.0):
     points = []
     for _ in range(nb_billes):
