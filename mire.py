eab305d3ebc22ec010d1742375404b8b5de3e574
Haha892 <haiban-aladel.alshouni@etu.univ-grenoble-alpes.fr>
2026-04-22 11:30:56 +0200

    cône + cube

--- mire.py
+++ mire.py
@@ -46,3 +46,33 @@ class Mire:
             points.append([pt["x"], pt["y"], pt["z"]])
 
         return cls(points, ids, data.get("name", "mire"))
+    
+    @classmethod
+    def generer_cone_tronque(cls, nb_billes, rayon_base=100.0, rayon_sommet=50.0, hauteur=30.0):
+        points = []
+        for _ in range(nb_billes):
+
+            z = np.random.uniform(0, hauteur)
+
+            rayon_max_z = rayon_base + (z / hauteur) * (rayon_sommet - rayon_base)
+            r = rayon_max_z * np.sqrt(np.random.uniform(0, 1))
+            theta = np.random.uniform(0, 2 * np.pi)
+
+            x = r * np.cos(theta)
+            y = r * np.sin(theta)
+            
+            points.append([x, y, z])
+        return cls(points)
+    
+    @classmethod
+    def generer_cube(cls, nb_billes, largeur=200.0, longueur=200.0, epaisseur=30.0):
+        points = []
+
+        for _ in range(nb_billes):
+            x = np.random.uniform(-largeur / 2, largeur / 2)
+            y = np.random.uniform(-longueur / 2, longueur / 2)
+            z = np.random.uniform(0, epaisseur)
+            
+            points.append([x, y, z])   
+        return cls(points)
+    
