b47cbe79e2d03be2fecfaa2105144171d4e619cf
ClemBelaid <clementine.belaid@etu.univ-grenoble-alpes.fr>
2026-04-29 14:54:18 +0200

    Réarrangement de matching.py

--- identifier.py
+++ identifier.py
@@ -1,5 +1,6 @@
-from core import Mire
-from core.matching import identification
+from src.core import Mire
+from src.core import Observation
+from src.core.matching import identification
 import sys
 
 if __name__ == "__main__":
