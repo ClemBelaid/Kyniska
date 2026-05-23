b3c81518c86b8b140ff9d2cc87e1a08694e5652d
Ymskc <rayane.bawa04@gmail.com>
2026-05-23 16:33:58 +0100

    Nouvelle fonction dans process qui fait les process pour tous les couples et le compare.py pour l'appliquer. Encore quelques incohérences au niveau de la frame renvoyée et du véritable angle dans generer2.py à voir

--- generer_2.py
+++ generer_2.py
@@ -31,14 +31,23 @@ if __name__ == "__main__":
     # Pour la GT, il faut enregistrer directement les identifiants de chaque point au moment de les projeter
     # Et pour les projections "anonymes" il faut enregistrer des IDs "random"
     # ou bien négatifs (pour se souvenir qu'ils représentent une valeur fausse/indéterminée)
-    tht = np.pi/32 # angle de 10 degrés
+    tht = np.pi/15 # angle de 10 degrés
     phi = tht # peu importe c'est pour tester 
     #mat1 = np.array([[np.cos(tht),0,np.sin(tht),30],[0,1,0,0],[-np.sin(tht),0,np.cos(tht),0],[0,0,0,1]]) # rotation et translation de la mire (frst_process simulé artficiellement)
-    
-    mat = np.array([
-    [np.cos(phi), -np.sin(phi), 0, 20],
+    axis = vrMire.points[0] - vrMire.points[1]
+    axis = axis / np.linalg.norm(axis)
+    mat_rot = calcul_matrice_rotation(axis , tht)
+
+    """mat_rot = np.array([
+    [np.cos(phi), -np.sin(phi), 0, 0],
     [np.sin(phi),  np.cos(phi), 0, 0],
     [0, 0, 1, 0],
+    [0, 0, 0, 1]
+    ])"""
+    mat_tr = np.array([
+    [1,0, 0, 0],
+    [ 0 ,1, 0, 0],
+    [0, 0, 1,0 ],
     [0, 0, 0, 1]
     ])
     u1, u2 = build_basis(v2)
@@ -61,16 +70,26 @@ if __name__ == "__main__":
     [0, 0, 0, 1]
     ])"""
 
-    lst = {}
+    lst_pts_tr = {}
+    lst_pts_rote = {}
+
     for id, x_mire in vrMire.pts.items():
-        x_mire_homo = np.array([x_mire[0], x_mire[1], x_mire[2], 1.0])
-        x_mire_trs_homo1= mat @ x_mire_homo
-        #x_mire_trs_homo2= mat2 @  x_mire_trs_homo1
-        lst[id] = x_mire_trs_homo1[:3].tolist()
-    points = list(lst.values())
-    ids = list(lst.keys())
-    mir=Mire(points, ids=ids, alignes=vrMire.alignes)
-    mir.save_json("Mire_tr")
-    obs_ref = proj.project_mire_to_plane(mir,screen)
+        #1ère transformation : rotation selon l'axe ym_xm
+        x_vec = np.array(x_mire)
+        x_rot = mat_rot @ x_vec
+        lst_pts_rote[id] = x_rot.tolist()
+        x_rot_h = np.array([x_rot[0],x_rot[1],x_rot[2],1.0])
+        x_tr_h= mat_tr @ x_rot_h
+        lst_pts_tr[id] = x_tr_h[:3].tolist()
+        
+    points = list(lst_pts_tr.values())
+    ids = list(lst_pts_tr.keys())
+    # mire originale sauvegardée
+    vrMire.save_json("Mire_tr")
+
+    # mire tournée utilisée seulement pour créer les observations
+    mir_rot = Mire(points, ids=ids, alignes=vrMire.alignes)
+
+    obs_ref = proj.project_mire_to_plane(mir_rot, screen)
     obs_ref.save_json("obs_ref")
     
--- src/core/process.py
+++ src/core/process.py
@@ -9,22 +9,19 @@ from src.core.projection import project_pt_to_plane
 from src.core.transformation import calcul_matrice_rotation
 from src.core.geometry import perpendicular_vector
 from src.core.check_for_process import check_observ
-from src.core.check_for_process import check_couple
+from itertools import combinations
+#from src.core.check_for_process import check_couple
 #from .visualiser6 import visualiser_iteration
 
 
 
-def frst_process(mire,screen,xo,yo):
-    """ Entrées: la mire , le vecteur normal du plan écran et les 2 points fixés de l'écran
-        Sorties:La mire obtenue après la translation et la rotation  les points xm, ym """
-    eps = 1e-2
-   
-    random.seed(0)
-    
+def frst_process(mire,screen,xm,ym,xo,yo):
+    """ Entrées: la mire , le plan écran(un dico avec vecteur normal, vect directeurs et origine), 2 points de la mire,et les 2 points fixés de l'écran
+        Sorties: La mire obtenue après la translation et la rotation  les points xm, ym apres translation et rotation et la liste des points transformés  """
     #On suppose que d(xo,yo)>0 (xo et yo ne sont pas trop proches)
     #prendre 2 points au hasard de notre mire 
     vn=screen["normal"]
-    (xm,ym)=check_couple(mire,screen,xo,yo,1)
+    
     xp = project_pt_to_plane(xm, screen)
     yp = project_pt_to_plane(ym, screen)
     #A ce stade on a un couple (xm,ym) candidat potentiel à la projection de (xo,yo)
@@ -104,7 +101,7 @@ def frst_process(mire,screen,xo,yo):
 def scd_process(mire,screen,lst_xmr_fin,xm,ym,xo,yo):
     """Entrées: la mire , le vecteur normal au plan écran ,la liste des points de la mire après le first_process, les points 
     xm et ym obtenues après translation et rotation  de la mire, xo et yo les points fixés de l'écran
-       Sorties : la mire roté pour que p(ym)=yp soit égale à yo """
+       Sorties : la mire roté pour que p(ym)=yp soit égale à yo, les points xm , ym tranformés par cette deuxième rotation et la liste des pts transformés """
     ym_xm = ym - xm
     yo_xo = yo - xo #un vecteur 2D qu'il faut mettre en 3D avec le repère (u1 ,u2 ) de l'écran 
    
@@ -144,7 +141,7 @@ def scd_process(mire,screen,lst_xmr_fin,xm,ym,xo,yo):
     return (Mire(points, ids=ids, alignes=mire.alignes), xm_rote , ym_rote, lst_fin)
 
 def thd_process(mire,screen,obs,xm,ym,N):
-    """Entrées: la mire qui a été fixée correctement,le vecteur normal au plan ,l'observation originale, 
+    """Entrées: la mire qui a été fixée correctement,l'écran en dico avec ses 4 attrributs : vect normal , origine et vect directeurs, les pts xm, ym bien fixés et N constante de discrétisation de l'intervalle des angles 
     xm et ym qui sont sur notre axe de rotation N pour la discrétisation des angles """
     
     angles = np.linspace(0, 2*np.pi, N, endpoint=False)
@@ -154,8 +151,8 @@ def thd_process(mire,screen,obs,xm,ym,N):
     eps = 1e-4
 
     best_rms = np.inf
-    best_mire = None
-    best_angle = None
+    #best_mire = None
+    #best_angle = None
 
     for agl in angles:
 
@@ -183,72 +180,92 @@ def thd_process(mire,screen,obs,xm,ym,N):
 
         if rms < best_rms:
             best_rms = rms
-            best_mire = new_mire
-            best_angle = agl
+            #best_mire = new_mire
+            #best_angle = agl
 
         if rms < eps:
             break
 
-        return (best_mire, best_rms, best_angle)
-           
+    return  best_rms, agl
+    """( best_mire,"""
+    """, best_angle)"""
 
-
-"""def thd_process_aff(mire,screen,obs,xm,ym,N):
-    Entrées: la mire qui a été fixée correctement,le vecteur normal au plan ,l'observation originale, 
-    xm et ym qui sont sur notre axe de rotation N pour la discrétisation des angles 
-    angles = np.linspace(0, 2*np.pi, N, endpoint=False)
-    ym_xm = ym - xm
-
-    eps = 1e-4
-
-    best_rms = np.inf
-    best_mire = None
-    best_angle = None
-
-    plt.ion()
-
-    fig = plt.figure(figsize=(10,8))
-    ax = fig.add_subplot(111, projection='3d')
-
-    for agl in angles:
-
-
-        axis = ym_xm / np.linalg.norm(ym_xm)
-        mR = calcul_matrice_rotation(axis, agl)
-
-        lst_pt = {}
-
-        for id, x_mire in mire.pts.items():
-            x_mire = np.array(x_mire)
-
-            x_mire_rote = mR @ (x_mire - xm) + xm
-
-            lst_pt[id] = x_mire_rote.tolist()
-
-        points = list(lst_pt.values())
-        ids = list(lst_pt.keys())
-
-        new_mire = Mire(points, ids=ids, alignes=mire.alignes)
-
-        observ = project_mire_to_plane(new_mire, screen)
-        observ = project_mire_to_plane(new_mire, screen)
-        rms = check_observ(obs, observ)
-        visualiser_iteration(
-            new_mire,
-            observ,
-            screen,
-            angle=agl,
-            rms=rms
-        )
-
+def app_proc(mire,obs,screen,xo,yo): 
         
+    pts_items = list(mire.pts.items())
+    d_obs = np.linalg.norm(yo - xo)
+    best_score = np.inf
+    best_mire = None
+    best_xm = None
+    best_ym = None
+    best_agl = 0
 
-        if rms < best_rms:
-            best_rms = rms
-            best_mire = new_mire
-            best_angle = agl
+    for (id1, xm), (id2, ym) in combinations(pts_items, 2):
 
-        if rms < eps:
-            break
+        xm = np.array(xm)
+        ym = np.array(ym)
+         # projections des points candidats
+        xp = project_pt_to_plane(xm, screen)
+        yp = project_pt_to_plane(ym, screen)
 
-    return (best_mire, best_rms, best_angle)"""
+        d_proj = np.linalg.norm(yp - xp)
+
+        # conditions
+        if d_proj <= 1e-8:
+                continue
+
+        if d_proj < d_obs:
+                continue
+
+        try:
+
+                # process 1
+                mire1, xm1, ym1, lst1 = frst_process(
+                    mire,
+                    screen,
+                    xm,
+                    ym,
+                    xo,
+                    yo
+                )
+
+                # process 2
+                mire2, xm2, ym2, lst2 = scd_process(
+                    mire1,
+                    screen,
+                    lst1,
+                    xm1,
+                    ym1,
+                    xo,
+                    yo
+                )
+
+                # process 3
+                score , agl = thd_process(
+                    mire2,
+                    screen,
+                    obs,
+                    xm2,
+                    ym2,
+                    360
+                )
+
+                # meilleur résultat
+                if score < best_score:
+
+                    best_score = score
+                    best_agl = agl
+                    best_mire = mire2
+                    best_xm = xm
+                    best_ym = ym
+
+        except Exception as e:
+
+                print("Erreur couple :", id1, id2, e)
+
+                continue
+
+    return best_mire, best_xm, best_ym, best_score , best_agl
+
+
+
