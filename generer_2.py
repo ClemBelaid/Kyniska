8035e0c1ca761e58a532380f144bef9ab5bcbf6c
ClemBelaid <clementine.belaid@etu.univ-grenoble-alpes.fr>
2026-05-27 14:37:40 +0200

    fonction compare.py corrigée

--- generer_2.py
+++ generer_2.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     # Pour la GT, il faut enregistrer directement les identifiants de chaque point au moment de les projeter
     # Et pour les projections "anonymes" il faut enregistrer des IDs "random"
     # ou bien négatifs (pour se souvenir qu'ils représentent une valeur fausse/indéterminée)
-    tht = np.pi/6 # angle de 10 degrés
+    tht = np.pi/4 # angle de 10 degrés
     phi = tht # peu importe c'est pour tester 
     #mat1 = np.array([[np.cos(tht),0,np.sin(tht),30],[0,1,0,0],[-np.sin(tht),0,np.cos(tht),0],[0,0,0,1]]) # rotation et translation de la mire (frst_process simulé artficiellement)
-    axis = vrMire.points[0] - vrMire.points[1]
+    axis = vrMire.points[2] - vrMire.points[4]
     axis = axis / np.linalg.norm(axis)
     mat_rot = calcul_matrice_rotation(axis , tht)
     xm = vrMire.points[0]
