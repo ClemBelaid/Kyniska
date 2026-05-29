ab4d2530ae541cda27f13b79ef305de474404bcb
ClemBelaid <clementine.belaid@etu.univ-grenoble-alpes.fr>
2026-05-29 16:02:46 +0200

    test

--- src/core/compare.py
+++ src/core/compare.py
@@ -25,7 +25,7 @@ SAVE_GIF = False
 # 3D scene -- Data
 #--------------------------------
 
-mire = Mire.load_json("Mire_init") # Notre mire initiale (non-rotée) et l'algo va déterminer cette rotation qui colle avec les observations de obs_ref
+mire = Mire.load_json("vrMire") # Notre mire initiale (non-rotée) et l'algo va déterminer cette rotation qui colle avec les observations de obs_ref
 mire_rot = Mire.load_json("Mire_rot") # La mire tournée et translatée ayant servi à calculer obs_ref
 obs_ref = Observation.load_json("obs_ref")
 obs_pts = obs_ref.points
